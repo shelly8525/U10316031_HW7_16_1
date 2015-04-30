ea4d95a1098b06f753682148247c18ca658a6c0e
shelly8525 <shelly8525@yahoo.com.tw>
2015-04-30 23:33:04 +0800

    把撲克牌換成課本的
    改好程式碼,從把panel直接換掉改成把圖片換掉

--- src/PickFourCards.java
+++ src/PickFourCards.java
@@ -5,60 +5,65 @@ import java.util.*;
 
 public class PickFourCards extends JFrame{
 	private JButton jbtRefresh = new JButton("Refresh");
+	private JLabel a; 
+	private JLabel b; 
+	private JLabel c; 
+	private JLabel d; 
+	
 	JPanel panel = new JPanel();
 	
-	private ImageIcon image1 = new ImageIcon("src/c1.png"); 
-	private ImageIcon image2 = new ImageIcon("src/c2.png");
-	private ImageIcon image3 = new ImageIcon("src/c3.png");
-	private ImageIcon image4 = new ImageIcon("src/c4.png");
-	private ImageIcon image5 = new ImageIcon("src/c5.png");
-	private ImageIcon image6 = new ImageIcon("src/c6.png");
-	private ImageIcon image7 = new ImageIcon("src/c7.png");
-	private ImageIcon image8 = new ImageIcon("src/c8.png");
-	private ImageIcon image9 = new ImageIcon("src/c9.png");
-	private ImageIcon image10 = new ImageIcon("src/c10.png");
-	private ImageIcon image11 = new ImageIcon("src/c11.png");
-	private ImageIcon image12 = new ImageIcon("src/c12.png");
-	private ImageIcon image13 = new ImageIcon("src/c13.png");
-	private ImageIcon image14 = new ImageIcon("src/d1.png");
-	private ImageIcon image15 = new ImageIcon("src/d2.png");
-	private ImageIcon image16 = new ImageIcon("src/d3.png");
-	private ImageIcon image17 = new ImageIcon("src/d4.png");
-	private ImageIcon image18 = new ImageIcon("src/d5.png");
-	private ImageIcon image19 = new ImageIcon("src/d6.png");
-	private ImageIcon image20 = new ImageIcon("src/d7.png");
-	private ImageIcon image21 = new ImageIcon("src/d8.png");
-	private ImageIcon image22 = new ImageIcon("src/d9.png");
-	private ImageIcon image23 = new ImageIcon("src/d10.png");
-	private ImageIcon image24 = new ImageIcon("src/d11.png");
-	private ImageIcon image25 = new ImageIcon("src/d12.png");
-	private ImageIcon image26 = new ImageIcon("src/d13.png");
-	private ImageIcon image27 = new ImageIcon("src/h1.png");
-	private ImageIcon image28 = new ImageIcon("src/h2.png");
-	private ImageIcon image29 = new ImageIcon("src/h3.png");
-	private ImageIcon image30 = new ImageIcon("src/h4.png");
-	private ImageIcon image31 = new ImageIcon("src/h5.png");
-	private ImageIcon image32 = new ImageIcon("src/h6.png");
-	private ImageIcon image33 = new ImageIcon("src/h7.png");
-	private ImageIcon image34 = new ImageIcon("src/h8.png");
-	private ImageIcon image35 = new ImageIcon("src/h9.png");
-	private ImageIcon image36 = new ImageIcon("src/h10.png");
-	private ImageIcon image37 = new ImageIcon("src/h11.png");
-	private ImageIcon image38 = new ImageIcon("src/h12.png");
-	private ImageIcon image39 = new ImageIcon("src/h13.png");
-	private ImageIcon image40 = new ImageIcon("src/s1.png");
-	private ImageIcon image41 = new ImageIcon("src/s2.png");
-	private ImageIcon image42 = new ImageIcon("src/s3.png");
-	private ImageIcon image43 = new ImageIcon("src/s4.png");
-	private ImageIcon image44 = new ImageIcon("src/s5.png");
-	private ImageIcon image45 = new ImageIcon("src/s6.png");
-	private ImageIcon image46 = new ImageIcon("src/s7.png");
-	private ImageIcon image47 = new ImageIcon("src/s8.png");
-	private ImageIcon image48 = new ImageIcon("src/s9.png");
-	private ImageIcon image49 = new ImageIcon("src/s10.png");
-	private ImageIcon image50 = new ImageIcon("src/s11.png");
-	private ImageIcon image51 = new ImageIcon("src/s12.png");
-	private ImageIcon image52 = new ImageIcon("src/s13.png");
+	private ImageIcon image1 = new ImageIcon("src/1.png"); 
+	private ImageIcon image2 = new ImageIcon("src/2.png");
+	private ImageIcon image3 = new ImageIcon("src/3.png");
+	private ImageIcon image4 = new ImageIcon("src/4.png");
+	private ImageIcon image5 = new ImageIcon("src/5.png");
+	private ImageIcon image6 = new ImageIcon("src/6.png");
+	private ImageIcon image7 = new ImageIcon("src/7.png");
+	private ImageIcon image8 = new ImageIcon("src/8.png");
+	private ImageIcon image9 = new ImageIcon("src/9.png");
+	private ImageIcon image10 = new ImageIcon("src/10.png");
+	private ImageIcon image11 = new ImageIcon("src/11.png");
+	private ImageIcon image12 = new ImageIcon("src/12.png");
+	private ImageIcon image13 = new ImageIcon("src/13.png");
+	private ImageIcon image14 = new ImageIcon("src/14.png");
+	private ImageIcon image15 = new ImageIcon("src/15.png");
+	private ImageIcon image16 = new ImageIcon("src/16.png");
+	private ImageIcon image17 = new ImageIcon("src/17.png");
+	private ImageIcon image18 = new ImageIcon("src/18.png");
+	private ImageIcon image19 = new ImageIcon("src/19.png");
+	private ImageIcon image20 = new ImageIcon("src/20.png");
+	private ImageIcon image21 = new ImageIcon("src/21.png");
+	private ImageIcon image22 = new ImageIcon("src/22.png");
+	private ImageIcon image23 = new ImageIcon("src/23.png");
+	private ImageIcon image24 = new ImageIcon("src/24.png");
+	private ImageIcon image25 = new ImageIcon("src/25.png");
+	private ImageIcon image26 = new ImageIcon("src/26.png");
+	private ImageIcon image27 = new ImageIcon("src/27.png");
+	private ImageIcon image28 = new ImageIcon("src/28.png");
+	private ImageIcon image29 = new ImageIcon("src/29.png");
+	private ImageIcon image30 = new ImageIcon("src/30.png");
+	private ImageIcon image31 = new ImageIcon("src/31.png");
+	private ImageIcon image32 = new ImageIcon("src/32.png");
+	private ImageIcon image33 = new ImageIcon("src/33.png");
+	private ImageIcon image34 = new ImageIcon("src/34.png");
+	private ImageIcon image35 = new ImageIcon("src/35.png");
+	private ImageIcon image36 = new ImageIcon("src/36.png");
+	private ImageIcon image37 = new ImageIcon("src/37.png");
+	private ImageIcon image38 = new ImageIcon("src/38.png");
+	private ImageIcon image39 = new ImageIcon("src/39.png");
+	private ImageIcon image40 = new ImageIcon("src/40.png");
+	private ImageIcon image41 = new ImageIcon("src/41.png");
+	private ImageIcon image42 = new ImageIcon("src/42.png");
+	private ImageIcon image43 = new ImageIcon("src/43.png");
+	private ImageIcon image44 = new ImageIcon("src/44.png");
+	private ImageIcon image45 = new ImageIcon("src/45.png");
+	private ImageIcon image46 = new ImageIcon("src/46.png");
+	private ImageIcon image47 = new ImageIcon("src/47.png");
+	private ImageIcon image48 = new ImageIcon("src/48.png");
+	private ImageIcon image49 = new ImageIcon("src/49.png");
+	private ImageIcon image50 = new ImageIcon("src/50.png");
+	private ImageIcon image51 = new ImageIcon("src/51.png");
+	private ImageIcon image52 = new ImageIcon("src/52.png");
 	
 	ArrayList<ImageIcon> list = new ArrayList<ImageIcon>();
 	
@@ -119,26 +124,29 @@ public class PickFourCards extends JFrame{
 		list.add(image52);
 		
 		Collections.shuffle(list);
-		panel.add(new JLabel(list.get(1)));
-		panel.add(new JLabel(list.get(2)));
-		panel.add(new JLabel(list.get(3)));
-		panel.add(new JLabel(list.get(4)));
+		a = new JLabel(list.get(1));
+		b = new JLabel(list.get(2));
+		c = new JLabel(list.get(3));
+		d = new JLabel(list.get(4));
+		
+		panel.add(a);
+		panel.add(b);
+		panel.add(c);
+		panel.add(d);
+		
 		
 		this.add(jbtRefresh,BorderLayout.SOUTH);
 		this.add(panel,BorderLayout.CENTER);
-		
-		
-		
-		
+				
 		jbtRefresh.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e){
-				panel.removeAll();
 				Collections.shuffle(list);
-				panel.add(new JLabel(list.get(1)));
-				panel.add(new JLabel(list.get(2)));
-				panel.add(new JLabel(list.get(3)));
-				panel.add(new JLabel(list.get(4)));
+				a.setIcon(list.get(1));
+				b.setIcon(list.get(2));
+				c.setIcon(list.get(3));
+				d.setIcon(list.get(4));
+				panel.repaint();
 			}
 		}
 		);
@@ -150,12 +158,11 @@ public class PickFourCards extends JFrame{
 		frame.setTitle("Pick Four Cards");
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 150);
+		frame.setSize(300, 180);
 		frame.setVisible(true);
 	}
 
 }
-	
 
 
 
